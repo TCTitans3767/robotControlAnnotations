b057a0fe4f2ad86597f4fbd6c4677a94f0b99726
Jacob <jcmertz@outlook.com>
2025-10-15 19:30:13 -0400

    added the actual annotations to the package

---
+++ src/main/java/Transition.java
@@ -0,0 +1,11 @@
+package frc.robot.utils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
+public @interface Transition {
+}
