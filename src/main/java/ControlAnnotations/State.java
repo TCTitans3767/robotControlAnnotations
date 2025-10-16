ebfde631ce5b4abaf57f4304d8b1fd1066266add
Jacob <jcmertz@outlook.com>
2025-10-16 19:03:12 -0400

    added documentation for each contained annotation and the robot controller

--- src/main/java/ControlAnnotations/State.java
+++ src/main/java/ControlAnnotations/State.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a class to be automatically generated into the RobotStates class.
+ * Every class marked with this annotation must extend {@link edu.wpi.first.wpilibj2.command.Command}
+ */
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.CONSTRUCTOR, ElementType.TYPE})
 public @interface State {
--- src/main/java/RobotController/RobotController.java
+++ src/main/java/RobotController/RobotController.java
@@ -4,6 +4,11 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * The base robot state controller that will be the centerpoint of all robot functions.
+ * The {@link RobotController} statically contains the current {@link Command} that the robot is running.
+ * This {@link Command} can be changed by calling {@link RobotController#setCurrentMode(Command)}.
+ */
 public class RobotController extends SubsystemBase {
 
     /**
@@ -23,6 +28,12 @@ public class RobotController extends SubsystemBase {
 
     }
 
+    /**
+     * Sets the current {@link Command} to the given {@link Command}.
+     * The new {@link Command} will be scheduled if the current {@link Command} is different from the given {@link Command}
+     * The new {@link Command} must directly control only the robot's superstructure mechanism, not the drivetrain.
+     * @param newMode
+     */
     public static void setCurrentMode(Command newMode) {
         if (currentMode != newMode && currentMode != null) {
             currentMode.cancel();
